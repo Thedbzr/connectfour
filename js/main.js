b9030ddd938c7a4df8fb14b1b0d70ad398c8615a
Anthony Martinez <Martinezanthony213@gmail.com>
2020-11-09 08:49:17 -0800

    completes wireframing for gameBoard

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 /*----- constants -----*/
+const PLAYERS = {
+    '1': 'red',
+    '-1': 'yellow',
+    'null': 'white'
+}
+
+
+
+
+
 /*----- app's state (variables) -----*/
+let gameBoard;
+let winner;
+let turn;
+
+
 /*----- cached element references -----*/
+const cells = document.querySelectorAll('.cell');
+const message = document.querySelector('#message')
+const resetBtn = document.querySelector('#resetBtn')
+
+
+
 /*----- event listeners -----*/
+document.getElementById('gameBoard').addEventListener('click',handleClick);
+document.getElementById('resetBtn').addEventListener('click',init);
+
+
+
+
 /*----- functions -----*/
+init()
+
+function init() {
+    winner = null;
+    turn = 1;
+    render()
+}
+
+function render() {
+
+}
+
+

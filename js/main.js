8bb1fc9f5257b6c0f01dda696c305c550ff1ca03
Anthony Martinez <Martinezanthony213@gmail.com>
2020-11-09 10:18:16 -0800

    handle clicks

--- js/main.js
+++ js/main.js
@@ -33,13 +33,67 @@ document.getElementById('resetBtn').addEventListener('click',init);
 init()
 
 function init() {
+    gameBoard = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
+    ]
     winner = null;
     turn = 1;
     render()
 }
 
 function render() {
+    //button hidden on init
+    resetBtn.style.display = 'none';
+    //fill grid
+    gameBoard.forEach(function(arr){
+        arr.forEach(function(cell,idx){
+            document.getElementById(idx).style.backgroundImage = cell && PLAYERS[cell];
+        })
+    })
 
+    // let checkTie = function(arr,search){
+
+    //     const nullCell = (element) => element = null;
+    //     return arr.some(row => row.includes(nullCell));
+    // }
+
+   let checkForNull = gameBoard.forEach(function(arr){
+        arr.forEach(function(cell){
+            
+        })
+    })
+
+    //check for winner using ternary
+    //check for tie
+    //no one won so change message turn
+    if(winner) {
+        message.textContent = `Player ${winner > 0 ? 1 : 2} wins!`
+    } else if (){
+        
+    }
 }
 
 
+function handleClick(evt){
+    // check if cell has player marker assigned to it
+    if(gameBoard[evt.target.id]) return;
+    //if anything but the cell has been clicked do nothing
+    if(evt.target.id === 'gameBoard') return;
+    //cell is not clickable it someone won
+    if(winner) return;
+    //let player to that cell
+    gameBoard[evt.target.id] = turn;
+    checkForWin();
+    //switch turn
+    turn *= -1;
+    render()
+}
+
+function checkForWin(){
+    
+}

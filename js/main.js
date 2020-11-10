7cc1a09e2cd26df642367b781e2135160eab1c28
Anthony Martinez <Martinezanthony213@gmail.com>
2020-11-09 21:03:33 -0800

    Gameboard Update arrays represent columns

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const LENGTH = 7;
 let gameBoard;
 let winner;
 let turn;
-let convertedArray;
+
 
 
 /*----- cached element references -----*/
@@ -35,14 +35,14 @@ init()
 
 function init() {
     gameBoard = [
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0]
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0]
     ]
-    convertedArray = [].concat(...gameBoard);
     winner = null;
     turn = 1;
     render()
@@ -52,16 +52,25 @@ function render() {
     //button hidden on init
     resetBtn.style.display = 'none';
     //fill grid
-    convertedArray.forEach(function (element, idx) {
-        document.getElementById(idx).style.backgroundImage = element && PLAYERS[element];
-    })
+    // gameBoard.forEach(function(arr){
+    //     arr.forEach(function(element,idx){
+    //         document.getElementById(idx).style.backgroundImage = element && PLAYERS[element];
+    //     })
+    // })
     //check for winner using ternary
     //check for tie
     //no one won so change message turn
+
+    let openCells = gameBoard.forEach(function(arr){
+        for(let i = 0;i < arr.length; i++){
+            arr.includes(null);
+        }
+    })
+
     if (winner) {
         message.textContent = `Player ${winner > 0 ? 1 : 2} wins!`
         resetBtn.style.display = null;
-    } else if (!convertedArray.includes(0)) {
+    } else if (openCells) {
         message.textContent = 'Bummer Its A Tie'
         resetBtn.style.display = null;
     } else {
@@ -72,20 +81,18 @@ function render() {
 
 function handleClick(evt) {
     // check if cell has player marker assigned to it
-    if (convertedArray[evt.target.id]) return;
+    if (gameBoard[[evt.target.id]]) return;
     //if anything but the cell has been clicked do nothing
     if (evt.target.id === 'gameBoard') return;
     //cell is not clickable it someone won
     if (winner) return;
     //let player to that cell
-    convertedArray[evt.target.id] = turn;
+    gameBoard[[evt.target.id]] = turn;
     checkForWin();
     //switch turn
     turn *= -1;
     render()
-    console.log(convertedArray);
-    console.log(evt.target.id%7);
-
+    console.log(evt.target);
 }
 
 function checkForWin() {
@@ -93,54 +100,34 @@ function checkForWin() {
 }
 
 
-// let column0 = [];
-// let column1 = [];
-// let column2 = [];
-// let column3 = [];
-// let column4 = [];
-// let column5 = [];
-// let column6 = [];
-// let column7 = [];
 
-// let counter = 0;
 
-// let columnCells = function(){
-//     for(let i = 0; i < convertedArray.length; i++){
-//         if([i] % 7 == 0){
-//             column0.push(i);
+
+// let checkCell = function(){
+//     gameBoard.forEach(function(arr){
+//         for(let i = 0; i < arr.length; i++){
+//             arr[i].id === evt.target.id
 //         }
-
-//     }
-// }
-
-// columnCells()
-// console.log(column0);
-
-
-
-// let columnCells = convertedArray.filter(el => el.length % 7 == 0);
-
-// console.log(columnCells);
-
-
-// let getColumns= function(){
-//     for(let i = 0; i < convertedArray.length; i++){
-//         if(convertedArray[i] % 7 == 0) {
-//             columnCells.push([i])
-//         }
-//     }
+//     })
 // }
 
 
-
-// let checkV = convertedArray.filter(function(cell){
-//     return cell.id % 1;
+// convertedArray.forEach(function (element, idx) {
+//     document.getElementById(idx).style.backgroundImage = element && PLAYERS[element];
 // })
-// console.log(checkV);
 
+// if (winner) {
+//     message.textContent = `Player ${winner > 0 ? 1 : 2} wins!`
+//     resetBtn.style.display = null;
+// } else if (!convertedArray.includes(0)) {
+//     message.textContent = 'Bummer Its A Tie'
+//     resetBtn.style.display = null;
+// } else {
+//     message.textContent = `Player ${turn > 0 ? 1 : 2}'s turn!`
+// }
 
-// let checkVertically = convertedArray.filter(function(cell){
-//     console.log(cell);
-//     return cell.id % LENGTH ===  0;
-// })
-// console.log(checkVertically);
+//  //fill grid
+//  gameBoard.forEach(function (element, idx) {
+//     document.getElementById(idx).style.backgroundImage = element && PLAYER[element];
+//   })
+

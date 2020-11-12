47b648477057637ebefe7b456f54a6c986a54739
Anthony Martinez <Martinezanthony213@gmail.com>
2020-11-11 20:29:55 -0800

    completes column and row win logic

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ let winner;
 let turn;
 let rowIdx;
 let colIdx;
+let colStack;
+
 
 
 /*----- cached element references -----*/
@@ -32,13 +34,13 @@ init()
 
 function init() {
     gameBoard = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0]
+        [0, 0, 0, 0, 0, 0], //col 1
+        [0, 0, 0, 0, 0, 0], //col 2
+        [0, 0, 0, 0, 0, 0], //col 3
+        [0, 0, 0, 0, 0, 0], //col 4
+        [0, 0, 0, 0, 0, 0], //col 5
+        [0, 0, 0, 0, 0, 0], //col 6
+        [0, 0, 0, 0, 0, 0]  //col 7
     ]
     winner = null;
     turn = 1;
@@ -59,8 +61,8 @@ function render() {
     //check for tie
     //no one won so change message turn
     let openCells = function () {
-        gameBoard.forEach(function(arr){
-            arr.forEach(function(element){
+        gameBoard.forEach(function (arr) {
+            arr.forEach(function (element) {
 
             })
         })
@@ -89,14 +91,14 @@ function handleClick(evt) {
     var coordinate = evt.target.id.replace(/\D/g, '');
     colIdx = parseInt(coordinate.charAt(0));
     rowIdx = parseInt(coordinate.charAt(1));
-
     //set tokens to stack
     const colArr = gameBoard[colIdx];
-    rowIdx = colArr.indexOf(0);
-
-
-    if(rowIdx == -1) return;
     console.log(rowIdx);
+    
+
+    colStack = colArr.indexOf(0);
+    if (colStack == -1) return;
+
 
     //cell is not clickable it someone won
     if (winner) return;
@@ -104,17 +106,17 @@ function handleClick(evt) {
 
     //loop through gameBoard
     for (let i = 0; i < gameBoard.length; i++) {
-        let index1 = i
         for (let j = 0; j < gameBoard[i].length; j++) {
-            let index2 = j
             //link player to that cell
-            if (gameBoard[colIdx][rowIdx] == 0) {
-                gameBoard[colIdx][rowIdx] = turn;
+            if (gameBoard[colIdx][colStack] == 0) {
+                gameBoard[colIdx][colStack] = turn;
             }
         }
     }
 
     checkForWin();
+    checkCoulmns(colArr);
+    checkRows(rowIdx)
     //switch turn
     turn *= -1;
     render()
@@ -123,3 +125,30 @@ function handleClick(evt) {
 function checkForWin() {
 
 }
+
+function checkCoulmns(colArr) {
+    for (let i = 0; i < 3; i++) {
+        //loop through column that was clicked    
+        //check 4 cells at a time
+        if (colArr[i] !== 0 && colArr[i] == colArr[i + 1] && colArr[i] == colArr[i + 2] && colArr[i] == colArr[i + 3]) {
+            winner = turn;
+            //console.log(`winner is ${turn}`)
+        }
+    }
+}
+
+function checkRows(rowIdx) {
+    for (let i = 0; i < 4; i++) {
+        // console.log(gameBoard[i][rowIdx]);
+        // console.log(gameBoard[i+1][rowIdx]);
+        // console.log(gameBoard[i+2][rowIdx]);
+        // console.log(gameBoard[i+3][rowIdx]);
+        // console.log('-------end----------');
+
+        if ( gameBoard[i][rowIdx] !== 0 && gameBoard[i][rowIdx] == gameBoard[i + 1][rowIdx] && gameBoard[i][rowIdx] == gameBoard[i + 2][rowIdx] &&   gameBoard[i][rowIdx] == gameBoard[i + 3][rowIdx]) {
+            winner = turn;
+            console.log('row win')
+        }
+    }
+}
+
